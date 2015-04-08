55ea1ed7a504333d9bb4969f5f737da02f7a073c
Kian <sixx1101@gmail.com>
2015-04-08 14:40:38 +1000

    change format to top50 streamers

--- pokergraph_userspit.py
+++ pokergraph_userspit.py
@@ -10,16 +10,16 @@ clientid = 'bgpmqt0ei7713q8qy4ys70p448ni8q'
 clientsecret = '549n7s4y3k75oh45g8cxxx9hnx6z4sv'
 
 def connect_to():
-        url = 'https://api.twitch.tv/kraken/search/streams?query=Poker&live=true'
+        url = 'https://api.twitch.tv/kraken/streams?limit=50'
         return url
 
 def twitch_parse(data):
 	for item in data['streams']: #check the json responce for the item details
 		twitch_user = item['channel']['display_name']
-		influxdb_name = "twitch.user."+twitch_user
+		influxdb_name = "twitch.user."+twitch_user+".current_view"
 		current_viewer = item['viewers']
 		json_body = [{"points":[[current_viewer]],"name":influxdb_name,"columns":["value"]}]
-		db = InfluxDBClient('74.82.3.100', 8086, 'poker_user', 'pokergraph', 'pokergraph')
+		db = InfluxDBClient('74.82.3.100', 8086, 'twitch_graphing', 'twitch_graph_user', 'twitchgraph')
 		db.write_points(json_body)
 
 def submit_url(url):
